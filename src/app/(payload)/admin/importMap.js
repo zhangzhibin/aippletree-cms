28592c85bc11c57fb0124ac60b90d168ad0b7c7b
zhangzhibin@home <zhangzhibin@gmail.com>
2025-05-23 22:12:21 +0800

    使用vecel postgres和cf r2

--- src/app/(payload)/admin/importMap.js
+++ src/app/(payload)/admin/importMap.js
@@ -3,11 +3,13 @@ import { QueryPresetsWhereCell as QueryPresetsWhereCell_3817bf644402e67bfe6577f6
 import { QueryPresetsWhereField as QueryPresetsWhereField_3817bf644402e67bfe6577f60ef982de } from '@payloadcms/ui'
 import { QueryPresetsColumnsCell as QueryPresetsColumnsCell_3817bf644402e67bfe6577f60ef982de } from '@payloadcms/ui'
 import { QueryPresetsColumnField as QueryPresetsColumnField_3817bf644402e67bfe6577f60ef982de } from '@payloadcms/ui'
+import { S3ClientUploadHandler as S3ClientUploadHandler_f97aa6c64367fa259c5bc0567239ef24 } from '@payloadcms/storage-s3/client'
 
 export const importMap = {
   "@payloadcms/ui#QueryPresetsAccessCell": QueryPresetsAccessCell_3817bf644402e67bfe6577f60ef982de,
   "@payloadcms/ui#QueryPresetsWhereCell": QueryPresetsWhereCell_3817bf644402e67bfe6577f60ef982de,
   "@payloadcms/ui#QueryPresetsWhereField": QueryPresetsWhereField_3817bf644402e67bfe6577f60ef982de,
   "@payloadcms/ui#QueryPresetsColumnsCell": QueryPresetsColumnsCell_3817bf644402e67bfe6577f60ef982de,
-  "@payloadcms/ui#QueryPresetsColumnField": QueryPresetsColumnField_3817bf644402e67bfe6577f60ef982de
+  "@payloadcms/ui#QueryPresetsColumnField": QueryPresetsColumnField_3817bf644402e67bfe6577f60ef982de,
+  "@payloadcms/storage-s3/client#S3ClientUploadHandler": S3ClientUploadHandler_f97aa6c64367fa259c5bc0567239ef24
 }
--- src/payload.config.ts
+++ src/payload.config.ts
@@ -6,7 +6,10 @@ import path from 'path'
 import { buildConfig } from 'payload'
 import { fileURLToPath } from 'url'
 import sharp from 'sharp'
-import { getCustomPosts, getSimplifiedPosts } from './routes/posts'
+import { vercelPostgresAdapter } from '@payloadcms/db-vercel-postgres'
+import { s3Storage } from '@payloadcms/storage-s3'
+
+// import { getCustomPosts, getSimplifiedPosts } from './routes/posts'
 
 import { Users } from './collections/Users'
 import { Media } from './collections/Media'
@@ -32,15 +35,37 @@ export default buildConfig({
   typescript: {
     outputFile: path.resolve(dirname, 'payload-types.ts'),
   },
-  db: sqliteAdapter({
-    client: {
-      url: process.env.DATABASE_URI || '',
+  // db: sqliteAdapter({
+  //   client: {
+  //     url: process.env.DATABASE_URI || '',
+  //   },
+  // }),
+  db: vercelPostgresAdapter({
+    // 自动使用 process.env.POSTGRES_URL，如果没有提供选项
+    // 或者您可以手动指定连接字符串
+    pool: {
+      connectionString: process.env.DATABASE_URL, // 使用 PostgreSQL 连接字符串
     },
   }),
   sharp,
   plugins: [
-    payloadCloudPlugin(),
-    // storage-adapter-placeholder
+    s3Storage({
+      collections: {
+        media: true, // 指定使用 R2 存储的集合
+        // 'media-with-prefix': {
+        //   prefix: 'my-prefix', // 可选：为上传的文件添加前缀
+        // },
+      },
+      bucket: process.env.R2_BUCKET, // R2 存储桶名称
+      config: {
+        credentials: {
+          accessKeyId: process.env.R2_ACCESS_KEY_ID, // R2 访问密钥 ID
+          secretAccessKey: process.env.R2_SECRET_ACCESS_KEY, // R2 秘密访问密钥
+        },
+        region: 'auto', // R2 区域
+        endpoint: process.env.R2_ENDPOINT, // R2 端点
+      },
+    }),
   ],
   // routes: {
   //   api: {
